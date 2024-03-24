36a6ba844f671a6eff667d3ca116ba4b58a30bc2
WETLION <67816135+WETLION@users.noreply.github.com>
2024-03-24 04:38:35 +0000

    Create 가장 긴 증가하는 부분 수열.cpp

---
+++ BOJ/가장 긴 증가하는 부분 수열.cpp
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <cstdio>
+#include <iostream>
+#include <vector>
+
+// 11053번 가장 긴 증가하는 부분 수열 1
+
+using namespace std;
+
+vector<int> v, vv;
+// int arr[1000001];
+
+int main() {
+  int n, input, location;
+  // scanf("%d", &n);
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    // scanf("%d", &arr[i]);
+    cin >> input;
+    v.push_back(input);
+  }
+  for (int i = 0; i < n; i++) {
+    if (vv.empty() || vv.back() < v[i])
+      vv.push_back(v[i]);
+    // location = upper_bound(arr, arr+n, input)-arr;
+    location = lower_bound(vv.begin(), vv.end(), v[i]) - vv.begin();
+    vv[location] = v[i];
+  }
+  cout << vv.size();
+
+  return 0;
+}
