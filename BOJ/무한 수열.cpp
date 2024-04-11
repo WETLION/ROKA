552bc854749f801c68b99a50caf7636e0150d11f
WETLION <67816135+WETLION@users.noreply.github.com>
2024-04-11 09:53:32 +0000

    Update 무한 수열.cpp

--- BOJ/무한 수열.cpp
+++ BOJ/무한 수열.cpp
@@ -1,47 +1,29 @@
 #include <algorithm>
 #include <iostream>
-#include <vector>
+#include <map>
 
 // 1351번 무한 수열
 
 using namespace std;
-
-struct node {
-  int num, cnt;
-};
-
-vector<node> v;
 long long int n, p, q;
 
-bool cmp(node a, node b) { return a.cnt < b.cnt; }
-
-int node_cmp(int a, int b) { return a == b ? 0 : a > b ? 1 : -1; }
-
-long long int find_location(long long int l, long long int r,
-                            long long int find) {
-  if (l > r)
-    return -1;
-  long long int m = (l + r) / 2;
-  int cmp = node_cmp(find, v[m].cnt);
-  return cmp == 0   ? m
-         : cmp == 1 ? find_location(m + 1, r, find)
-                    : find_location(l, m - 1, find);
-}
+map<long long int, long long int> m;
 
 long long int find(long long int num) {
-  long long int cmp = find_location(0, v.size() - 1, num);
-  if (cmp != -1)
-    return v[cmp].num;
-  long long int v_num = find(num / p) + find(num / q);
-  v.push_back({v_num, num});
-  return v_num;
+  // cout << num << endl;
+  if (m.find(num) != m.end())
+    return m[num];
+  long long int number = find(num / p) + find(num / q);
+  m.insert({num, number});
+  return number;
 }
 
 int main() {
   cin >> n >> p >> q;
-  v.push_back({1, 0});
+  m.insert({0, 1});
+  // cout << "#";
   cout << find(n);
-  cin >> n;
+  // cin >> n;
 
   return 0;
 }
