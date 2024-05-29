00d149907baedc8a9fa4df6c1bfa99ed570403ed
WETLION <67816135+WETLION@users.noreply.github.com>
2024-05-29 04:26:07 +0000

    Create 균형 잡힌 세상.cpp

---
+++ BOJ/균형잡힌 세상.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+// 4949번 균형잡힌 세상
+
+using namespace std;
+
+string input;
+
+bool check_input(int front) {
+
+  return 1;
+}
+
+int main() {
+  while(1) {
+    cin >> input;
+    if(input == ".") break;
+    cout << (check_input(0) ? "yes" : "no") << endl;  
+  }
+
+  return 0;
+}
