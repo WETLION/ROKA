24608d6466b743a6d4838c3c7b4053f5113bdf70
WETLION <67816135+WETLION@users.noreply.github.com>
2024-06-04 11:10:47 +0000

    Create 균형잡힌 세상.cpp

--- BOJ/균형잡힌 세상.cpp
+++ BOJ/균형잡힌 세상.cpp
@@ -5,18 +5,39 @@
 
 using namespace std;
 
+int location;
 string input;
 
-bool check_input(int front) {
-
-  return 1;
+bool balance(int type) {
+  while(location < input.size()) {
+    if(input[location] == '(') {
+      location++;
+      if(!balance(1)) return 0;
+    }
+    else if(input[location] == '[') {
+      location++;
+      if(!balance(-1)) return 0;
+    }
+    else if(input[location] == ')') {
+      if(type != 1) return 0;
+      return 1;
+    }
+    else if(input[location] == ']') {
+      if(type != -1) return 0;
+      return 1;
+    }
+    location++;
+  }
+  return type ? 0 : 1;
 }
 
 int main() {
   while(1) {
-    cin >> input;
+    getline(cin, input);
+    // cout << input << endl;
     if(input == ".") break;
-    cout << (check_input(0) ? "yes" : "no") << endl;  
+    location = 0;
+    cout << (balance(0) ? "yes" : "no") << endl;
   }
 
   return 0;
