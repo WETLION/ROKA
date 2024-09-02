d30909efeee5da78bdf973a887c89afcb55a85e2
WETLION <67816135+WETLION@users.noreply.github.com>
2024-09-02 11:09:56 +0000

    Create SciComLove.cpp

---
+++ BOJ/SciComLove.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+// 21598번 SciComLove
+
+using namespace std;
+
+int n;
+
+int main() {
+    cin >> n;
+    while(n--) {
+        cout << "SciComLove" << endl;
+    }
+
+    return 0;
+}
