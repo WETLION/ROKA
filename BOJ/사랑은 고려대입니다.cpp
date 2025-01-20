fb1ce26c06165a5fa588661fd1df73bc216e6386
WETLION <67816135+WETLION@users.noreply.github.com>
2025-01-20 09:46:17 +0000

    Create 사랑은 고려대입니다.cpp

---
+++ BOJ/사랑은 고려대입니다.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+// 32384번 사랑은 고려대입니다
+
+using namespace std;
+
+int n;
+
+int main() {
+    cin >> n;
+    while(n--) {
+        cout << "LoveisKoreaUniversity ";
+    }
+
+    return 0;
+}
